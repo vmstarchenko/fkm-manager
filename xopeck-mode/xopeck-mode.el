9035c05103e181a4d096bb4c7478598b2c2dfac1
vladimir <xopeck@gmail.com>
2016-10-24 22:51:24 +0300

    add autodetection of /modes folder

--- xopeck-mode/xopeck-mode.el
+++ xopeck-mode/xopeck-mode.el
@@ -1,7 +1,13 @@
-(add-to-list 'load-path "~/.emacs.d/xopeck-mode/modes")
+;; (add-to-list 'load-path "~/.emacs.d/xopeck-mode/modes")
 ;; (add-to-list 'load-path
 ;;              (concat (file-name-directory (buffer-file-name (current-buffer)))
 ;;                      "modes")) ;; doesnt work
+(add-to-list 'load-path
+             (expand-file-name
+              (concat
+               (file-name-directory (locate-library "xopeck-mode"))
+               ;; or (file-truename "xopeck-mode")
+               "/modes")))
 
 ;; Моды
 (require 'xop-modes)
