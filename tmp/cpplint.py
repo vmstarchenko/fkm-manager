2e7cd4597113c59a43c49930aa3f830fbd8a428a
vladimir <vmstarchenko@edu.hse.ru>
2016-11-28 15:57:35 +0300

    correct with results from sd

--- tmp/cpplint.py
+++ tmp/cpplint.py
@@ -1,15 +1,15 @@
---2016-11-18 18:30:31--  https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
-Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 151.101.36.133
-Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|151.101.36.133|:443... connected.
-HTTP request sent, awaiting response... 200 OK
-Length: 237261 (232K) [text/plain]
-Saving to: ‘cpplint.py’
+--2016-11-28 15:55:14--  https://raw.githubusercontent.com/google/styleguide/gh-pages/cpplint/cpplint.py
+Распознаётся raw.githubusercontent.com (raw.githubusercontent.com)... 151.101.36.133
+Подключение к raw.githubusercontent.com (raw.githubusercontent.com)|151.101.36.133|:443... соединение установлено.
+HTTP-запрос отправлен. Ожидание ответа... 200 OK
+Длина: 237261 (232K) [text/plain]
+Сохранение в каталог: ««cpplint.py»».
 
-     0K .......... .......... .......... .......... .......... 21%  358K 1s
-    50K .......... .......... .......... .......... .......... 43%  829K 0s
-   100K .......... .......... .......... .......... .......... 64% 1,54M 0s
-   150K .......... .......... .......... .......... .......... 86% 2,28M 0s
-   200K .......... .......... .......... .                    100% 1,90M=0,3s
+     0K .......... .......... .......... .......... .......... 21%  476K 0s
+    50K .......... .......... .......... .......... .......... 43% 1,61M 0s
+   100K .......... .......... .......... .......... .......... 64% 2,57M 0s
+   150K .......... .......... .......... .......... .......... 86% 3,37M 0s
+   200K .......... .......... .......... .                    100% 4,20M=0,2s
 
-2016-11-18 18:30:32 (860 KB/s) - ‘cpplint.py’ saved [237261/237261]
+2016-11-28 15:55:15 (1,28 MB/s) - «cpplint.py» сохранён [237261/237261]
 
